0e4fc4154140eb1b56eb7706d3fdecf9dfdfb52a
Agata Pacholczyk <agataa.pacholczyk@gmail.com>
2020-06-25 16:53:02 +0200

    add send order to Api[16.4]

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -11,16 +11,27 @@ import {calculateTotal} from '../../../utils/calculateTotal';
 import settings from '../../../data/settings.js';
 
 
-const sendOrder = (options, tripCost, tripName,  tripId, counrtyCode ) => {
+const sendOrder = (options, tripCost, tripName,  tripId, tripCode ) => {
   const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const {contact, name} = options;
+
+  if(name === '' || name.lenght < 2) {
+    alert('Please, fill name`s field');
+    return;
+  }
+  if(contact === '' || contact.lenght < 4) {
+    alert ('Please, fill contact`s field');
+    return;
+  }
+
 
   const payload = {
     ...options,
     totalCost,
-    name,
     tripName,
     tripId,
-    counrtyCode,
+    tripCode,
   };
 
   const url = settings.db.url + '/' + settings.db.endpoint.orders;
@@ -42,23 +53,26 @@ const sendOrder = (options, tripCost, tripName,  tripId, counrtyCode ) => {
     });
 };
 
-const OrderForm = ({tripCost, options, setOrderOption }) => (
+const OrderForm = ({tripCost, options, tripName,  tripId, tripCode, setOrderOption }) => (
   <Row>
     {pricing.map(option => (
       <Col md={4} key={option.id}>
-        <OrderOption {...option} currentValue={options[option.id]} setOrderOption={setOrderOption}/>
+        <OrderOption {...option} currentValue={options[option.id]} setOrderOption={setOrderOption} />
       </Col> 
     ))}
     <Col xs={12}>
       <OrderSummary tripCost={tripCost} options={options} />
     </Col>
-    <Button onClick={() => sendOrder(options, tripCost, )}>Order now!</Button>
+    <Button onClick={() => sendOrder(options, tripCost, tripName,  tripId, tripCode )}>Order now!</Button>
   </Row>
 );
 OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  tripName: PropTypes.string,
+  tripId: PropTypes.string,
+  tripCode: PropTypes.string,
   
 };
 
